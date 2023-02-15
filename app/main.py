6797e4b7d85c6aada664ba2e66ef0642059fe07a
elmiringos <elmir.iskhakov1337@gmail.com>
2023-02-15 17:03:41 +0300

    change port to 6379

--- app/main.py
+++ app/main.py
@@ -3,7 +3,7 @@ import socket
 
 def main():
     print("Logs from your program will appear here!")
-    server_socket = socket.create_server(("localhost", 4000), reuse_port=True)
+    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client, _ = server_socket.accept()
     client.recv(1024)
     client.send(b"+PONG\r\n")
