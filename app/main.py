a57d09b2d9549dc07bb6b7bfe155d9b4b07334ae
elmiringos <elmir.iskhakov1337@gmail.com>
2023-02-15 17:00:28 +0300

    stage #2

--- app/main.py
+++ app/main.py
@@ -3,9 +3,10 @@ import socket
 
 def main():
     print("Logs from your program will appear here!")
-
-    server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
+    server_socket = socket.create_server(("localhost", 4000), reuse_port=True)
+    client, _ = server_socket.accept()
+    client.recv(1024)
+    client.send(b"+PONG\r\n")
 
 
 if __name__ == "__main__":
