479fc33c8304eef2f39719e779c02c944d5063e2
elmiringos <elmir.iskhakov1337@gmail.com>
2023-02-16 22:47:13 +0300

    stage #4

--- app/main.py
+++ app/main.py
@@ -1,4 +1,14 @@
 import socket
+import threading
+
+
+def handle_connection(client_connection):
+    while True:
+        try:
+            client_connection.recv(1024)  # wait for client to send data
+            client_connection.send(b"+PONG\r\n")
+        except ConnectionError:
+            break  # Stop serving if the client connection is closed
 
 
 def main():
@@ -6,9 +16,9 @@ def main():
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
     client, _ = server_socket.accept()
     while True:
-        client.recv(1024)
-        client.send(b"+PONG\r\n")
-
+        client_connection, _ = server_socket.accept()  # wait for client
+        threading.Thread(target=handle_connection, args=(client_connection,)).start()
+     
 
 if __name__ == "__main__":
     main()
